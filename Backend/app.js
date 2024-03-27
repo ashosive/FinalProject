be0b321fa70422724958cf11c1300312e026bea7
Ash Osive <65212969+ashosive@users.noreply.github.com>
2024-03-27 18:39:43 -0400

    Develop (#3)
    * backend setup WIP  & dependency fix
    
    * express basic code
    
    * tailwind add
    
    * code refactor
    
    * backend updated

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,15 @@
 // app.js
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/mern_database', {
+})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 // Define a route
 app.get('/', (req, res) => {
