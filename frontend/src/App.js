813e2988a3d0099d178614abd21b524a22c13bb9
Ashwani Maan <65212969+ashosive@users.noreply.github.com>
2024-04-03 18:18:30 -0400

    login page (#4)
    * login page
    
    * update
    
    ---------
    
    Co-authored-by: Riddhi1622 <90517553+Riddhi1622@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
+
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+   <div>
+  <Login />
+   </div>
   );
 }
 
